fd8c78812f48c3fd6ec9ec47329d8d286d16d86e
architvis <architvis@gmail.com>
2021-01-05 15:22:00 -0500

    changed interval method. Went from setInterval that went to lowest interval of 1 ms and made a new interval method using built in function immediate and then only step if min time has passed.

--- spicerack_simulation.js
+++ spicerack_simulation.js
@@ -3,6 +3,11 @@ var express = require('express')
 var router = express.Router()
 var spiceRack = require("./spicerack_configurations/config1");
 
+const NS_PER_SEC = 1e9;
+const MUS_PER_SEC = 1e6;
+const MS_PER_SEC = 1e3;
+const SEC_PER_NS = 1e-9;
+
 var inventory = spiceRack.inventory;
 
 const rState = {
@@ -10,7 +15,11 @@ const rState = {
   ACTIVE: "active",        // communicating and taking requests
   ROTATING: "rotating",  // moving spice platform, cannot take certain requests
   AWAITINGINPUT: "awaiting user input"  // when rack needs to wait for user input to say a task is complete, like "User removed and put back the spice" 
-} 
+}
+
+function getstepsNs(stepsPerSecond){
+  return ((1/(stepsPerSecond))*NS_PER_SEC);
+}
 
 class TurnTable {
   constructor(rotation = 0, stepDegrees, stepsPerSecond, idle = true, state = rState.ACTIVE, fullRotation = true, socket) {
@@ -25,7 +34,7 @@ class TurnTable {
     this.socket = socket;
   }
 
-  run(){
+  run() {
 
   }
 
@@ -35,10 +44,10 @@ class TurnTable {
     return ms;
   }
 
-  setSteps(newRotation){
-    this.rotation = (this.rotation%360);
+  setSteps(newRotation) {
+    this.rotation = (this.rotation % 360);
     this.idle = false;
-    this.state = rState.ROTATING; 
+    this.state = rState.ROTATING;
 
     var dist = newRotation - this.rotation;
     if (Math.abs(dist) > 180) {
@@ -48,17 +57,43 @@ class TurnTable {
     this.steps = Math.abs(Math.round(dist / this.stepDegrees));
   }
 
-  step(){
-    this.rotation = this.rotation + (this.direction * this.stepDegrees);
-    this.steps--;
-  
-    if (this.steps > 0) {
-      this.socket.emit('turnTable_rotation', this.rotation);
-      setTimeout(() =>{ this.step() }, this.stepsPerMs);
-    } else {
-      this.idle = true;
-      this.state = rState.ACTIVE;
+  step() {
+    
+    
+    let startTime = process.hrtime.bigint();
+    let time = startTime;
+    let stepToggle= 0;
+    let nsSpeed = this.stepsPerMs * 1000000;
+    this.socket.emit('turnTable_rotation', this.rotation);
+    // setTimeout(() =>{ this.step() }, this.stepsPerMs);
+    function innerLoop(self) {
+      // console.log("innerLoop");
+        setImmediate(() => {
+          if (self.steps > 0) { // do if steps still exist
+            if(stepToggle == 1){ // put to low, fastest way
+              stepToggle=0;
+              innerLoop(self);
+              return;
+            }
+            let newTime = process.hrtime.bigint();
+            let diff = newTime - time;
+            if(diff > nsSpeed){ // step if true
+              self.steps--;
+              time = newTime;
+              self.rotation = self.rotation + (self.direction * self.stepDegrees);
+              self.socket.emit('turnTable_rotation', self.rotation);
+            }
+            innerLoop(self);  // innerLoop is always called if more steps exist
+
+          } else {
+            
+            self.idle = true;
+            self.state = rState.ACTIVE;
+          }
+        });
     }
+    innerLoop(this);
+    this.socket.emit('turnTable_rotation', this.rotation);
   }
 
 }
@@ -73,35 +108,35 @@ function main(io) {
 
   router.get('/movetopoint/:id', function (req, res) {
     var foundPoint = inventory.find(x => x.uid == req.params.id);
-    if(foundPoint != null & table.idle == true){
+    if (foundPoint != null & table.idle == true) {
       // the following should be inside the turn table class, likely in a beginStep function
       table.setSteps(foundPoint.rotation);
       table.step();
       res.send(200);
-    }else{
+    } else {
       res.send(500); // generic server error code
     }
   })
 
-  router.post('/setName', (req, res) =>{
+  router.post('/setName', (req, res) => {
     var foundPoint = inventory.find(x => x.uid == req.body.id);
-    if(foundPoint != null & table.idle == true){
-      foundPoint.name = req.body.name; 
+    if (foundPoint != null & table.idle == true) {
+      foundPoint.name = req.body.name;
       io.emit('turnTable_inventory', inventory);
       res.send(200);
-    }else{
+    } else {
       console.error("Point not found");
       res.send(500); // generic server error code
     }
   })
 
-  router.post('/setContents', (req, res) =>{
+  router.post('/setContents', (req, res) => {
     var foundPoint = inventory.find(x => x.uid == req.body.id);
-    if(foundPoint != null & table.idle == true){
-      foundPoint.contents = req.body.contents; 
+    if (foundPoint != null & table.idle == true) {
+      foundPoint.contents = req.body.contents;
       io.emit('turnTable_inventory', inventory);
       res.send(200);
-    }else{
+    } else {
       console.error("Point not found");
       res.send(500); // generic server error code
     }
